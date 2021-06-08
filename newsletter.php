787f18b178e296c04eef65287d7f8fd656717118
Manuel Castellano <cm.manuelcastellano@gmail.com>
2021-06-07 21:42:00 -0300

    Nuevamente subido. Verificar formulario

--- newsletter.php
+++ newsletter.php
@@ -1,22 +1,19 @@
 <?php
-$nombre = $_POST['nombre'];
 $mail = $_POST['email'];
-$empresa = $_POST['mensaje'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje = "Este mensaje fue enviado por " . $mail . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'bettina@viajarengrupo.com.ar';
-$asunto = 'Mensaje de mi sitio web';
+$para = 'viajarengrupoweb@gmail.com';
+$asunto = 'Me suscribi a tu newsletter, quiero info';
 
-mail($para, $asunto, utf8_decode($mensaje), $header);
+mail($para, $asunto, utf8_decode($mail), $header);
 
 header("Location:index.html");
 ?>
